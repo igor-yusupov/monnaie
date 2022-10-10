5cbcb9927541527578438745ec1907d7be24b71e
llacqie <llacqie@gmail.com>
2022-10-10 14:09:05 +0500

    Implement some params

--- monnaie/wallet/params.rs
+++ monnaie/wallet/params.rs
@@ -109,7 +109,7 @@ pub struct Destination {
 
 #[derive(Debug, Deserialize_repr, Serialize_repr)]
 #[repr(u32)]
-pub enum Priority {
+pub enum TransferPriority {
 	Default,
 	Unimportant,
 	Normal,
@@ -121,7 +121,7 @@ pub(super) struct Transfer {
 	pub destinations: Vec<Destination>,
 	pub account_index: Option<u32>,
 	pub subaddr_indices: Option<Vec<u32>>,
-	pub priority: Priority,
+	pub priority: TransferPriority,
 	pub mixin: u32,
 	pub ring_size: u32,
 	pub unlock_time: u32,
@@ -140,9 +140,164 @@ pub(super) struct TransferSplit {
 	pub ring_size: u32,
 	pub unlock_time: u32,
 	pub get_tx_keys: Option<bool>,
-	pub priority: Priority,
+	pub priority: TransferPriority,
 	pub do_not_relay: Option<bool>,
 	pub get_tx_hex: Option<bool>,
 	pub new_algorithm: Option<bool>,
 	pub get_tx_metadata: Option<bool>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct SignTransfer {
+	pub unsigned_txset: String,
+	pub export_raw: Option<bool>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct SubmitTransfer {
+	pub tx_data_hex: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct SweepDust {
+	pub get_tx_keys: Option<bool>,
+	pub do_not_relay: Option<bool>,
+	pub get_tx_hex: Option<bool>,
+	pub get_tx_metadata: Option<bool>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct SweepAll {
+	pub address: String,
+	pub account_index: u32,
+	pub subaddr_indices: Option<Vec<u32>>,
+	pub priority: Option<u32>,
+	pub mixin: u32,
+	pub ring_size: u32,
+	pub unlock_time: u32,
+	pub get_tx_keys: Option<bool>,
+	pub below_amount: Option<u32>,
+	pub do_not_relay: Option<bool>,
+	pub get_tx_hex: Option<bool>,
+	pub get_tx_metadata: Option<bool>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct SweepSingle {
+	pub address: String,
+	pub account_index: u32,
+	pub subaddr_indices: Option<Vec<u32>>,
+	pub priority: Option<u32>,
+	pub mixin: u32,
+	pub ring_size: u32,
+	pub unlock_time: u32,
+	pub get_tx_keys: Option<bool>,
+	pub key_image: String,
+	pub below_amount: Option<u32>,
+	pub do_not_relay: Option<bool>,
+	pub get_tx_hex: Option<bool>,
+	pub get_tx_metadata: Option<bool>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct RelayTx {
+	pub hex: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct GetPayments {
+	pub payment_id: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct GetBulkPayments {
+	pub payment_ids: Vec<String>,
+	pub min_block_height: u32,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum TransferType {
+	All,
+	Available,
+	Unavailable,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct IncomingTransfers {
+	pub transfer_type: TransferType,
+	pub account_index: Option<u32>,
+	pub subaddr_indices: Option<Vec<u32>>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum KeyType {
+	Mnemonic,
+	ViewKey,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct QueryKey {
+	pub key_type: KeyType,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct MakeIntegratedAddress {
+	pub standard_address: Option<String>,
+	pub payment_id: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct SplitIntegratedAddress {
+	pub integrated_address: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct SetTxNotes {
+	pub txids: Vec<String>,
+	pub notes: Vec<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct GetTxNotes {
+	pub txids: Vec<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct SetAttribute {
+	pub key: String,
+	pub value: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct GetAttribute {
+	pub key: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct GetTxKey {
+	pub txid: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct CheckTxKey {
+	pub txid: String,
+	pub tx_key: String,
+	pub address: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct GetTxProof {
+	pub txid: String,
+	pub address: String,
+	pub message: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(super) struct CheckTxProof {
+	pub txid: String,
+	pub address: String,
+	pub message: Option<String>,
+	pub signature: String,
 }
